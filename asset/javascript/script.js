ce12a12f774edfaaa16b49258580870911338087
Purvanas <arnaudmirocha@gmail.com>
2022-12-10 20:47:41 +0100

    navigation téléphone

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -1,5 +1,8 @@
 var slide = new Array("symfony/accueilSymfony.png", "symfony/connectionSymfony.png", "symfony/contactSymfony.png", "symfony/creationCompte.png");
 var numero = 0;
+var cpt=0;
+
+
 
 function ChangeSlide(sens) {
     numero = numero + sens;
@@ -8,4 +11,34 @@ function ChangeSlide(sens) {
     if (numero > slide.length - 1)
         numero = 0;
     document.getElementById("slide").src = slide[numero];
+}
+
+function sleep(ms) {
+    return new Promise(
+      resolve => setTimeout(resolve, ms)
+    );
+  }
+
+
+async function showNav(){
+    document.getElementById("boutonDeNav").classList.toggle('loaded'); 
+    if(cpt%2 == 0){
+        await sleep(500)
+        document.getElementById("nav-link-button-div").style.display = "contents";
+        cpt++;
+    }
+    else{
+        document.getElementById("nav-link-button-div").style.display = "none";
+        cpt++;
+    }
+}
+
+
+
+window.onresize = function(){
+    if(window.screen.width >= 991){
+    document.getElementById("boutonDeNav").style.height = "0";
+    document.getElementById("nav-link-button-div").style.display = "none";
+    cpt=0;
+  }
 }
