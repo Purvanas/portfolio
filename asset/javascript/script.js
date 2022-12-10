b767051c518a409d1c248ea95ccd3101793a4c56
Purvanas <arnaudmirocha@gmail.com>
2022-12-10 21:55:43 +0100

    navigation téléphone + smooth et header n+1

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -23,12 +23,31 @@ function sleep(ms) {
 async function showNav(){
     document.getElementById("boutonDeNav").classList.toggle('loaded'); 
     if(cpt%2 == 0){
-        await sleep(500)
         document.getElementById("nav-link-button-div").style.display = "contents";
+        await sleep(120)
+        document.getElementById('link1').style.opacity = "1";
+        await sleep(120)
+        document.getElementById('link2').style.opacity = "1";
+        await sleep(120)
+        document.getElementById('link3').style.opacity = "1";
+        await sleep(120)
+        document.getElementById('link4').style.opacity = "1";
+        await sleep(120)
+        document.getElementById('link5').style.opacity = "1";
         cpt++;
     }
-    else{
-        document.getElementById("nav-link-button-div").style.display = "none";
+    else{    
+        document.getElementById('link5').style.opacity = "0"; 
+        await sleep(100)
+        document.getElementById('link4').style.opacity = "0";
+        await sleep(100) 
+        document.getElementById('link3').style.opacity = "0"; 
+        await sleep(100)
+        document.getElementById('link2').style.opacity = "0";  
+        await sleep(100)       
+        document.getElementById('link1').style.opacity = "0";      
+        await sleep(120) 
+        document.getElementById("nav-link-button-div").style.display = "none";   
         cpt++;
     }
 }
